9ef031e3ac11311d9792747bf183475e095a324d
Yogindra <yogindrachaudhari816@gmail.com>
2024-04-04 17:18:52 +0530

    learnt about the creation of react app using nodejs using npm

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <h1>Learning React || How to create React project using npm : use "npm/npx create-react-app app-name" using node js
+      & How to create react project using vite : "npm create vite@latest" then add project name "eg_project" then select your 
+      framework or library and language.
+      How to use package.json to run the project.
+    </h1>
+  );
+}
+
+export default App;
